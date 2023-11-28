cdde3912d702bc40e0949941f987159b158058bf
Chhuong Le <chhuongle@gmail.com>
2023-11-27 19:03:00 -0800

    install shadcn, add authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import { UserButton } from "@clerk/nextjs";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox Clone</h1>
-      <UserButton />
     </main>
   )
 }
